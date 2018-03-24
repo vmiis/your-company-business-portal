dfaad2c3d0d5308de182b4e15a54ea22ddad2d69
Zhiming Zhou <zzm.vmiis@gmail.com>
2018-03-25 10:54:02 +1100

    update

--- index.js
+++ index.js
@@ -185,11 +185,16 @@ var vm_init=function(){
 		$.getScript('https://www.gstatic.com/charts/loader.js',function(){
 			google.charts.load('current', {packages: ['corechart']});
 		});
+		//-------------------------------------
         $vm.module_list['_system_export_dialog_module']={table_id:'',url:'__COMPONENT__/dialog/export_dialog_module.html'};
         $vm.load_module_by_name('_system_export_dialog_module','',{})
         //-------------------------------------
         $vm.module_list['_system_import_dialog_module']={table_id:'',url:'__COMPONENT__/dialog/import_dialog_module.html'};
         $vm.load_module_by_name('_system_import_dialog_module','',{})
+		//-------------------------------------
+		$vm.module_list['uploading_file_dialog_module']={table_id:'',url:'__COMPONENT__/dialog/uploading_file_dialog_module.html'};
+        $vm.load_module_by_name('_system_import_dialog_module','',{})
+		//-------------------------------------
 	}
 	//********************************************************
 	load_config_and_init();
