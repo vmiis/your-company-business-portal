4238ce0002065aacb87fa2fa3a6495db60a9ce63
Zhiming Zhou <zzm.vmiis@gmail.com>
2018-04-08 18:55:24 +1000

    update

--- isleephealth/copy.bat
+++ isleephealth/copy.bat
@@ -1,2 +1,3 @@
 xcopy %USERPROFILE%\Documents\GitHub\wappsystem\isleephealth\isleephealth\*.* %USERPROFILE%\Documents\GitHub\vmiis\your-company-business-portal\isleephealth\*.* /S /D
+xcopy %USERPROFILE%\Documents\GitHub\wappsystem\isleephealth\isleephealth-patients-portal\*.* %USERPROFILE%\Documents\GitHub\vmiis\your-company-business-portal\isleephealth-patients-portal\*.* /S /D
 pause
