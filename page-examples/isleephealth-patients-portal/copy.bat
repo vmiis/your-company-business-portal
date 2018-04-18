833468125e8cfec24188bc5979b9906fe2868287
vmiis <zzm.vmiis@gmail.com>
2018-04-18 17:30:18 +1000

    update

---
+++ page-examples/isleephealth-patients-portal/copy.bat
@@ -0,0 +1,2 @@
+xcopy %USERPROFILE%\Documents\GitHub\wappsystem\isleephealth-patients-portal\isleephealth-patients-portal\*.* %USERPROFILE%\Documents\GitHub\vmiis\your-company-business-portal\form-and-data-examples\isleephealth-patients-portal\*.* /S /D
+pause
