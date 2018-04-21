53adc2a403db52cb66b2e8b6c703937781ee9f19
Zhiming Zhou <zzm.vmiis@gmail.com>
2018-04-21 11:27:36 +1000

    update

--- page-examples/clinical-trials/task_data.js
+++ page-examples/clinical-trials/task_data.js
@@ -29,19 +29,19 @@ _set_req=function(){
     }
     var sql="with notes as (select PUID,NT=S1,NC=S2,NRowNum=row_number() over (PARTITION BY PUID order by ID DESC) from [FORM-"+notes_tid+"] where ppid="+_db_pid+")";
     sql+=",participant as (select Site=S1,ParticipantUID=UID,participant_sql="+participant_sql+" from [FORM-"+participant_tid+"]"+site_sql_where+participant_where+" )";
-    sql+=",task as (select ID,UID,PUID,S3,Site=participant.Site,Information,participant_sql,DateTime,Author,RowNum=row_number() over (order by ID DESC) from [FORM-"+_db_pid+"-@S1] join participant on PUID=ParticipantUID)";
+    sql+=",task as (select ID,UID,PUID,S3,Site=participant.Site,Information,participant_sql,DateTime,Author,RowNum=row_number() over (order by ID DESC) from [FORM-"+_db_pid+"-@S1] left join participant on PUID=ParticipantUID)";
     sql+="select Information,ID,S3,UID,Site,Participant=participant_sql,DateTime,Author,RowNum,NT,NC,dirty=0, valid=1 from task left join notes on UID=notes.PUID and NRowNum=1 where RowNum between @I6 and @I7";
     var sql_n="with participant as (select Site=S1,ParticipantUID=UID from [FORM-"+participant_tid+"]"+site_sql_where+" )";
-    sql_n+=" select count(ID) from [FORM-"+_db_pid+"-@S1] join participant on PUID=ParticipantUID";
+    sql_n+=" select count(ID) from [FORM-"+_db_pid+"-@S1] left join participant on PUID=ParticipantUID";
 
     _req={cmd:'query_records',sql:sql,sql_n:sql_n,s1:'"'+$('#keyword__ID').val()+'"',I:$('#I__ID').text(),page_size:$('#page_size__ID').val()}
 }
 //-------------------------------------
 _set_req_export=function(i1,i2){
-    _fields_e="Participant ID|ParticipantUID,Participant,"+_task_fields
+    _fields_e="Participant ID|ParticipantUID,Participant,"+fields
     var sql="with participant as (select Site=S1,ParticipantUID=UID,participant_sql="+participant_sql+" from [FORM-"+participant_tid+"]"+site_sql_where+" )";
     sql+=",task as (select ID,UID,PUID,S3,Information,DateTime,Author from [FORM-"+_db_pid+"-@S1])";
-    sql+=",records as (select ID,ParticipantUID,Site,Information,Participant=participant_sql,DateTime,Author,RowNum=row_number() over (order by ID DESC) from participant left join task on PUID=ParticipantUID)";
+    sql+=",records as (select ID,ParticipantUID,Site,Information,Participant=participant_sql,DateTime,Author,RowNum=row_number() over (order by ID DESC) from task left join participant on PUID=ParticipantUID)";
 	sql+=" select * from records where RowNum between @I1 and @I2";
     _req={cmd:'query_records',sql:sql,i1:i1,i2:i2}
 }
