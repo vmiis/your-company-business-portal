39fb53eb26c0419507d84d26612271f72e2b7de6
vmiis <zzm.vmiis@gmail.com>
2018-04-25 22:07:12 +1000

    update

--- pages/room-booking/task_form.js
+++ pages/room-booking/task_form.js
@@ -7,9 +7,14 @@ var prefix=this_module.prefix; if(prefix==undefined) prefix="";
 $('#D__ID').on('load',function(){
     $('#F__ID')[0].reset();
     $('#submit__ID').show();
-    var grid_record=$vm.vm['__ID'].op.record;
-    $('#delete__ID').hide(); if(grid_record!=undefined && grid_record.ID!==undefined) $('#delete__ID').show();
-    $vm.deserialize(grid_record,'#F__ID');
+    var input=$vm.vm['__ID'].op;
+    var input_record=input.record;
+    if(input_record==undefined){
+        input=input.input;
+        input_record=input.record;
+    }
+    $('#delete__ID').hide(); if(input_record!=undefined && input_record.ID!==undefined) $('#delete__ID').show();
+    $vm.deserialize(input_record,'#F__ID');
 })
 //-------------------------------------
 $('#F__ID').submit(function(event){
@@ -22,7 +27,17 @@ $('#F__ID').submit(function(event){
     if(typeof(before_submit)!=undefined) before_submit(data,dbv);
     //--------------------------------------------------------
     var db_pid=this_module.table_id;
-    var rid=undefined; if($vm.vm['__ID'].op.record!=undefined) rid=$vm.vm['__ID'].op.record.ID;
+    //-------------------------------------------------------
+    var rid=undefined;
+    var input=$vm.vm['__ID'].op;
+    var input_record=input.record;
+    if(input_record!=undefined) rid=input_record.ID;
+    if(rid==undefined){
+        input=input.input;
+        input_record=input.record;
+        rid=input_record.ID;
+    }
+    //-------------------------------------------------------
     var req={cmd:"add_json_record",db_pid:db_pid,data:data,dbv:dbv};
     if(rid!=undefined) req={cmd:"modify_json_record",rid:rid,db_pid:db_pid,data:data,dbv:dbv};
     $VmAPI.request({data:req,callback:function(res){
--- sales-tracking/copy.bat
+++
@@ -1,2 +0,0 @@
-xcopy %USERPROFILE%\Documents\GitHub\vmiis\applications\sales-tracking\sales-tracking\*.* %USERPROFILE%\Documents\GitHub\vmiis\your-company-business-portal\sales-tracking\*.* /S /D
-pause
