9b1eecb26224e960ee138b201e5200b7074d1c97
Zhiming Zhou <zzm.vmiis@gmail.com>
2018-04-28 11:47:06 +1000

    update

--- apps/medical-practitioner/index.js
+++ apps/medical-practitioner/index.js
@@ -1,37 +1,38 @@
 var vm_init=function(){
-	$vm.start_time=new Date().getTime();
-	//check and clear localstorage
-	var data=''; for(var key in window.localStorage){ if(window.localStorage.hasOwnProperty(key)){ data+=window.localStorage[key]; }}
-	if(data.length>3000000) localStorage.clear();
-	if(window.location.href.indexOf('?clearcache=1')!=-1){
-		localStorage.clear();
-		alert("Cache is cleard!");
-		return;
-	}
-	//set name space
-	$VmAPI={};$vm.module_list={};$vm.config_list={}
-	//--------------------------------------------------------
-	//get hosting path
-	var href = window.location.href.split('?')[0];
-	var path=href.split('/index.html')[0];
-	var lastChar=path[path.length-1];
-	if(lastChar=='/') path=path.substring(0,path.length-1);
-	$vm.hosting_path=path;
-	if(window.location.hostname=='127.0.0.1' || window.location.hostname=='localhost')	$vm.debug =true;
-	//--------------------------------------------------------
-	$vm.reload='';
-	if(window.location.toString().indexOf('_d=3')!=-1){
-		$vm.reload=new Date().getTime().toString();
-	}
-	$vm.version=$vm.ver[0];
-	//--------------------------------------------------------
-	var load_config_and_init=function(){
-		var url=$vm.hosting_path+"/configurations/index.json";
+	var start=function(){
+		//--------------------------------------------------------
+		$vm.start_time=new Date().getTime();
+		//check and clear localstorage
+		var data=''; for(var key in window.localStorage){ if(window.localStorage.hasOwnProperty(key)){ data+=window.localStorage[key]; }}
+		if(data.length>3000000) localStorage.clear();
+		if(window.location.href.indexOf('?clearcache=1')!=-1){
+			localStorage.clear();
+			alert("Cache is cleard!");
+			return;
+		}
+		//set name space
+		$VmAPI={};$vm.module_list={};$vm.config_list={}
+		//--------------------------------------------------------
+		//get hosting path
+		var href = window.location.href.split('?')[0];
+		var path=href.split('/index.html')[0];
+		var lastChar=path[path.length-1];
+		if(lastChar=='/') path=path.substring(0,path.length-1);
+		$vm.hosting_path=path;
+		if(window.location.hostname=='127.0.0.1' || window.location.hostname=='localhost')	$vm.debug =true;
+		//--------------------------------------------------------
+		$vm.reload='';
+		if(window.location.toString().indexOf('_d=3')!=-1){
+			$vm.reload=new Date().getTime().toString();
+		}
+		$vm.version=$vm.ver[0];
+		//--------------------------------------------------------
+		var url=$vm.hosting_path+"/index.json";
 		var ver=localStorage.getItem(url+"_ver");
 		var txt=localStorage.getItem(url+"_txt");
 		//------------------------------------------
 		if(ver!=$vm.ver[1] || txt===null || $vm.debug===true || $vm.reload!=''){
-			console.log('loading '+url+'?_='+$vm.ver[1]+$vm.reload);
+			console.log((new Date().getTime()-$vm.start_time).toString()+"---"+'loading '+url+'?_='+$vm.ver[1]+$vm.reload);
 			$.get(url+'?_='+$vm.ver[1]+$vm.reload,function(data){
 				localStorage.setItem(url+"_txt",data);
 				localStorage.setItem(url+"_ver",$vm.ver[1]);
@@ -64,6 +65,10 @@ var vm_init=function(){
 				$vm.server          ='production';
 				$VmAPI.api_base     =config.api_path_production;
 			}
+			else if(window.location.toString().indexOf('database=woolcock')!=-1){
+				$vm.server          ='production';
+				$VmAPI.api_base     =config.api_path_woolcock;
+			}
 			else{
 				$vm.server          ='development';
 				$VmAPI.api_base     =config.api_path_development;
@@ -89,7 +94,7 @@ var vm_init=function(){
 	var load_vm      =function(){ load_js($vm.url('https://vmiis.github.io/framework/distribution/vmframework.min.js'),init);}
 	var init         =function(){
 		$vm.init_v3({callback:function(){$vm.init_status=1;}})
-		$vm.load_first_module_to_body({url:'/modules/layout/main.html',callback:last});
+		$vm.load_first_module_to_body({url:'/layout/main.html'});
 	}
 	//--------------------------------------------------------
 	var load_js=function(url,next){
@@ -98,7 +103,7 @@ var vm_init=function(){
 		var txt=localStorage.getItem(url+"_txt");
 		//------------------------------------------
 		if(ver!=$vm.ver[2] || txt===null || $vm.debug===true || $vm.reload!=''){
-			console.log('loading '+url+'?_='+$vm.ver[2]+$vm.reload);
+			console.log((new Date().getTime()-$vm.start_time).toString()+"---"+'loading '+url+'?_='+$vm.ver[2]+$vm.reload);
 			$.get(url+'?_='+$vm.ver[2]+$vm.reload,function(data){
 				localStorage.setItem(url+"_txt",data);
 				localStorage.setItem(url+"_ver",$vm.ver[2]);
@@ -125,13 +130,19 @@ var vm_init=function(){
 			//use local version
 			text=text.replace(/https:\/\/cbs.wappsystem.com\/dev\/github/g,window.location.protocol+'//'+window.location.host);
 			text=text.replace(/https:\/\/cbs.wappsystem.com\/pro\/github/g,window.location.protocol+'//'+window.location.host);
-			text=text.replace(/https:\/\/distributed-modules.vmiis.com/g,window.location.protocol+'//'+window.location.host+'/vmiis/distributed-modules');
+			//text=text.replace(/https:\/\/distributed-modules.vmiis.com/g,window.location.protocol+'//'+window.location.host+'/vmiis/distributed-modules');
 
 			//do not use local system files
 			text=text.replace(/http:\/\/127.0.0.1:8000\/vmiis\/api/g,'https://vmiis.github.io/api');
 			text=text.replace(/http:\/\/127.0.0.1:8000\/vmiis\/framework/g,'https://vmiis.github.io/framework');
 			text=text.replace(/http:\/\/127.0.0.1:8000\/vmiis\/component/g,'https://vmiis.github.io/component');
 			text=text.replace(/http:\/\/127.0.0.1:8000\/vmiis\/modules/g,'https://vmiis.github.io/modules');
+
+			text=text.replace(/https:\/\/woolcock-imr.github.io/g,					window.location.protocol+'//'+window.location.host+'/woolcock-imr');
+			text=text.replace(/https:\/\/volunteer-database.rt.org.au/g,			window.location.protocol+'//'+window.location.host+'/woolcock-imr/volunteer-database-2');
+			text=text.replace(/https:\/\/volunteer-database-management.rt.org.au/g,	window.location.protocol+'//'+window.location.host+'/woolcock-imr/volunteer-database-management-2');
+			text=text.replace(/https:\/\/wappsystem.github.io/g,					window.location.protocol+'//'+window.location.host+'/wappsystem');
+
 		}
 		if(window.location.toString().indexOf('_d=1')!=-1){
 			//use local system files
@@ -152,33 +163,54 @@ var vm_init=function(){
 			text=text.replace(/http:\/\/127.0.0.1:8000\/vmiis\/component/g,'https://raw.githubusercontent.com/vmiis/component/master');
 			text=text.replace(/http:\/\/127.0.0.1:8000\/vmiis\/modules/g,'https://raw.githubusercontent.com/vmiis/modules/master');
 		}
+		text=text.replace(/https:\/\/woolcock-imr.github.io\/sfix\//g,'https://sfix.rt.org.au/');
+		text=text.replace(/https:\/\/woolcock-imr.github.io\/sfix\|/g,'https://sfix.rt.org.au|');
+		text=text.replace(/https:\/\/woolcock-imr.github.io\/sfix-online-questionnaire\//g,'https://sfix-online-questionnaire.rt.org.au/');
+		text=text.replace(/https:\/\/woolcock-imr.github.io\/sfix-online-questionnaire\|/g,'https://sfix-online-questionnaire.rt.org.au|');
 		return text;
 	}
-	//--------------------------------------------------------
-	var last=function(){
-		$('head').append("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css'>");
-        $('head').append("<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>");
-        $('head').append("<link rel='stylesheet' href='https://ajax.aspnetcdn.com/ajax/jquery.ui/1.12.1/themes/redmond/jquery-ui.css'>");
-		$.getScript('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js',function(){
-			$vm.js_popper=1;
-			$.getScript('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js',function(){
-				$vm.js_bootstrap=1;
-			});
-		});
-		$.getScript('https://ajax.aspnetcdn.com/ajax/jquery.ui/1.12.1/jquery-ui.min.js',function(){$vm.js_jquery_ui=1;});
-		$.getScript('https://apis.google.com/js/plusone.js');
-        $.getScript('https://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js');
-        $.getScript('https://sdk.amazonaws.com/js/aws-sdk-2.1.34.min.js');
-		$.getScript('https://www.gstatic.com/charts/loader.js',function(){
-			google.charts.load('current', {packages: ['corechart']});
-		});
-        $vm.module_list['_system_export_dialog_module']={table_id:'',url:'__PARTS__/dialog/export_dialog_module.html'};
-        $vm.load_module_by_name('_system_export_dialog_module','',{})
-        //-------------------------------------
-        $vm.module_list['_system_import_dialog_module']={table_id:'',url:'__PARTS__/dialog/import_dialog_module.html'};
-        $vm.load_module_by_name('_system_import_dialog_module','',{})
+	//------------------------------------
+	var resources=[
+		"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
+		"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+		"https://ajax.aspnetcdn.com/ajax/jquery.ui/1.12.1/themes/redmond/jquery-ui.css",
+
+		"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js",
+		"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js",
+		"https://ajax.aspnetcdn.com/ajax/jquery.ui/1.12.1/jquery-ui.min.js",
+		"https://apis.google.com/js/plusone.js",
+		"https://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js",
+		"https://sdk.amazonaws.com/js/aws-sdk-2.1.34.min.js",
+		"https://www.gstatic.com/charts/loader.js"
+	];
+	//------------------------------------
+	var load_resources=function(links){
+		for(i in links){
+			var e=links[i].split('.').pop();
+			if(e=='css'){
+				$('head').append("<link rel='stylesheet' href='"+links[i]+"'>");
+			}
+			else if(e=='js'){
+				load_js_link(links[i])
+			}
+		}
 	}
+	//------------------------------------
+	var load_js_link=function(link){
+		$.getScript(link,function(){
+			var nm=link.split('/').pop();
+			nm=nm.replace(/\./g,'-');
+			$vm[nm]=1;
+			if(nm=='loader-js'){
+				google.charts.load('current', {packages: ['corechart']});
+			}
+		});
+	}
+	//------------------------------------
+	setTimeout(function (){	$.ajaxSetup({cache:true}); load_resources(resources); },10);
+	//------------------------------------
+
 	//********************************************************
-	load_config_and_init();
+	start();
 	//********************************************************
 }
