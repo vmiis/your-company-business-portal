996dc9acdcab84b62aa1c00860a67627b41831cb
vmiis <zzm.vmiis@gmail.com>
2018-05-06 20:13:12 +1000

    update

---
+++ apps/_dev/create-form.js
@@ -0,0 +1,18 @@
+$('#toolbar__ID').on('dblclick',function(){
+    var txt="";
+    var flds=_fields.replace('_Form,','').replace(',Submit Date|DateTime,Submitted by|Author,_Delete','');
+    var A=[],B=[];
+    var s=flds.split(',');
+    for(var i=0;i<s.length;i++){
+        var L=s[i].split('|')[0].replace(/_/g,' ');
+        var N=s[i].split('|').pop();
+        txt+="<div class='row row__"+"ID'>\r\n";
+        txt+="<div class=col>\r\n";
+        txt+="<span>"+L+"</span>\r\n";
+        txt+="<input type=text class=form-control name="+N+">\r\n";
+        txt+="</div>\r\n";
+        txt+="</div>\r\n";
+    }
+    $vm.view_code(txt,"Form")
+})
+//-------------------------------------
