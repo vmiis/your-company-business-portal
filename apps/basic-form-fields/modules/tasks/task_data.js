664e0a2cd1312a66f4db9119f4cc5b7c8bd295e7
Zhiming Zhou <zzm.vmiis@gmail.com>
2018-07-01 09:53:28 +1000

    update

--- apps/basic-form-fields/modules/tasks/task_data.js
+++
@@ -1,9 +0,0 @@
-//-------------------------------------
-var this_module=$vm.module_list[$vm.vm['__ID'].name];
-var prefix=this_module.prefix; if(prefix==undefined) prefix="";
-var form_module=prefix+this_module.form_module;
-//-------------------------------------
-$('#new__ID').off('click').on('click',function(){$vm.load_module_v2(prefix+form_module,'',{goback:1})})
-//-------------------------------------
-$('#D__ID').on('load',function(){  m.set_req(); m.request_data(); })
-//-------------------------------------
